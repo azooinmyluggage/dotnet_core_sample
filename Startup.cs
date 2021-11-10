844cbdbfd393bbce9ff10d669641602d489513d4
Atul Malaviya <azooinmyluggage@users.noreply.github.com>
2021-11-10 15:44:29 -0600

    Update Startup.cs

--- Startup.cs
+++ Startup.cs
@@ -18,7 +18,7 @@ namespace pipelines_dotnet_core
         {
             Configuration = configuration;
         }
-
+ 
         public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
